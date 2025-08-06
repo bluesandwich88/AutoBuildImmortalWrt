ebc93182b1dd1aee6aeacc68e0f242baf03689b7
kevinCheng <lampard_cheng@hotmail.com>
2025-08-06 23:28:59 +0800

    big chanage

--- sunxi-cortexa53/build24.sh
+++ sunxi-cortexa53/build24.sh
@@ -1,53 +1,33 @@
 #!/bin/bash
 source shell/custom-packages.sh
-# 该文件实际为imagebuilder容器内的build.sh
-
-if [ -n "$CUSTOM_PACKAGES" ]; then
-  echo "✅ 你选择了第三方软件包：$CUSTOM_PACKAGES"
-  if [ "$PROFILE" = "glinet_gl-mt3000" ]; then
-    echo "❌ 检查到您集成了第三方软件包 由于mt3000闪存空间较小 不支持此操作"
-    echo "✅ 系统将自动帮你注释掉shell/custom-packages.sh中的插件 目前支持第三方插件集成的机型是mt2500/mt6000等大闪存机型"
-    CUSTOM_PACKAGES=""
-  else
-    # 下载 run 文件仓库
-    echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-    git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
-
-    # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
-    mkdir -p /home/build/immortalwrt/extra-packages
-    cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
-
-    echo "✅ Run files copied to extra-packages:"
-    ls -lh /home/build/immortalwrt/extra-packages/*.run
-    # 解压并拷贝ipk到packages目录
-    sh shell/prepare-packages.sh
-    ls -lah /home/build/immortalwrt/packages/
-    # 添加架构优先级信息
-    sed -i '1i\
-    arch aarch64_generic 10\n\
-    arch aarch64_cortex-a53 15' repositories.conf
-  fi
-else
-  echo "⚪️ 未选择任何第三方软件包"
-fi
-
-
-
+echo "第三方软件包: $CUSTOM_PACKAGES"
 # yml 传入的路由器型号 PROFILE
 echo "Building for profile: $PROFILE"
 echo "Include Docker: $INCLUDE_DOCKER"
-echo "Create pppoe-settings"
-mkdir -p  /home/build/immortalwrt/files/etc/config
+# yml 传入的固件大小 ROOTFS_PARTSIZE
+echo "Building for ROOTFS_PARTSIZE: $ROOTSIZE"
 
-# 创建pppoe配置文件 yml传入pppoe变量————>pppoe-settings文件
-cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
-enable_pppoe=${ENABLE_PPPOE}
-pppoe_account=${PPPOE_ACCOUNT}
-pppoe_password=${PPPOE_PASSWORD}
-EOF
+if [ -z "$CUSTOM_PACKAGES" ]; then
+  echo "⚪️ 未选择 任何第三方软件包"
+else
+  # 下载 run 文件仓库
+  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
+  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
 
-echo "cat pppoe-settings"
-cat /home/build/immortalwrt/files/etc/config/pppoe-settings
+  # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
+  mkdir -p /home/build/immortalwrt/extra-packages
+  cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
+
+  echo "✅ Run files copied to extra-packages:"
+  ls -lh /home/build/immortalwrt/extra-packages/*.run
+  # 解压并拷贝ipk到packages目录
+  sh shell/prepare-packages.sh
+  ls -lah /home/build/immortalwrt/packages/
+  # 添加架构优先级信息
+  sed -i '1i\
+  arch aarch64_generic 10\n\
+  arch aarch64_cortex-a53 15' repositories.conf
+fi
 
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
@@ -63,20 +43,16 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-#24.10.0
+
+#24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-# 增加几个必备组件 方便用户安装iStore
-PACKAGES="$PACKAGES fdisk"
-PACKAGES="$PACKAGES script-utils"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-
-# 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
+# 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 
@@ -101,12 +77,11 @@ else
     echo "⚪️ 未选择 luci-app-openclash"
 fi
 
-
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files"
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTSIZE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
